8d675b4795e7dd42184cf661c24d0235af79d691
Tony Km <kimaxhanthony9@gmail.com>
2023-11-28 15:06:35 +0300

    Create: Tooltips

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -3,21 +3,22 @@ import { FaMicrophone } from "react-icons/fa";
 
 const Search = () => {
   return (
-    <div className="flex gap-x-5 w-1/3">
-      <div className="w-full h-10 grid grid-cols-6 border border-white/30 rounded-full overflow-hidden">
-        <article className="col-span-5">
+    <div className="flex gap-x-5 w-2/4">
+      <div className="w-full h-10 grid grid-cols-7 border border-white/30 rounded-full overflow-hidden">
+        <article className="col-span-6">
           <input
             type="search"
             placeholder="Search"
             className="bg-transparent text-white pl-4 w-full h-full outline-none"
           />
         </article>
-        <button className="h-full px-5 bg-white/20">
+        <button className="h-full grid place-items-center bg-white/20">
           <IoIosSearch className="fill-white w-6 h-6" />
         </button>
       </div>
-      <button className="p-2 grid place-items-center bg-white/20 rounded-full">
-        <FaMicrophone className="fill-white w-6 h-6" />
+      <button className="p-3 grid place-items-center bg-white/20 rounded-full hover:bg-white/30 transition relative group">
+        <FaMicrophone className="fill-white w-5 h-5" />
+        <small className="text-white absolute -bottom-12 whitespace-nowrap px-4 py-1.5 bg-white/30 rounded scale-0 group-hover:scale-100 transition-all">Search with your voice</small>
       </button>
     </div>
   );
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -6,20 +6,22 @@ import { IoIosNotificationsOutline } from "react-icons/io";
 
 const Header = () => {
   return (
-    <header className="p-4 flex justify-between items-center">
+    <header className="py-2 px-5 flex justify-between items-center">
       <div className="flex items-center gap-x-4">
         <MenuBtn />
         <YoutubeLogo />
       </div>
       <Search />
-      <aside className="flex items-center">
-        <button className="p-2 hover:bg-white/20 rounded-full">
-          <BiVideoPlus className="w-7 h-7 fill-white"/>
+      <aside className="flex items-center gap-x-2">
+        <button className="p-2 hover:bg-white/20 rounded-full group relative">
+          <BiVideoPlus className="w-8 h-8 fill-white"/>
+          <small className="text-white absolute -bottom-12 left-2/4 -translate-x-2/4 whitespace-nowrap px-4 py-1.5 bg-white/30 rounded scale-0 group-hover:scale-100 transition-all">Create</small>
         </button>
-        <button className="p-2 hover:bg-white/20 rounded-full">
-          <IoIosNotificationsOutline className="w-7 h-7 fill-white"/>
+        <button className="p-2 hover:bg-white/20 rounded-full group relative">
+          <IoIosNotificationsOutline className="w-8 h-8 fill-white"/>
+          <small className="text-white absolute -bottom-12 left-2/4 -translate-x-2/4 whitespace-nowrap px-4 py-1.5 bg-white/30 rounded scale-0 group-hover:scale-100 transition-all">Notifications</small>
         </button>
-        <button className="w-9 h-9 rounded-full overflow-hidden">
+        <button className="w-10 h-10 rounded-full active:border border-green-500 overflow-hidden">
             <img className="object-cover" src="https://images.unsplash.com/photo-1701083266435-a56ea052fb60?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D" alt="Unsplash" />
         </button>
       </aside>
