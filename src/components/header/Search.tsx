3c437fd138e47e05b3462a10f311543e37e0cfd8
Tony Km <kimaxhanthony9@gmail.com>
2023-12-01 17:51:57 +0300

    Edit: Search bar"

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -4,15 +4,15 @@ import { FaMicrophone } from "react-icons/fa";
 const Search = () => {
   return (
     <div className="flex gap-x-5 w-2/4">
-      <div className="w-full h-10 grid grid-cols-7 border border-white/30 rounded-full overflow-hidden">
+      <div className="w-full h-10 grid grid-cols-7 rounded-full">
         <article className="col-span-6">
           <input
             type="search"
             placeholder="Search"
-            className="bg-transparent text-white pl-4 w-full h-full outline-none"
+            className="bg-transparent text-white pl-4 w-full h-full outline-none border border-white/30 focus:border-teal-700 rounded-l-full transition"
           />
         </article>
-        <button className="h-full grid place-items-center bg-white/20 hover:bg-white/30 transition">
+        <button className="h-full grid place-items-center rounded-r-full bg-white/30 hover:bg-white/30 transition">
           <IoIosSearch className="fill-white w-6 h-6" />
         </button>
       </div>
