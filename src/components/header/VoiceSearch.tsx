818c65d33f8c1ce2a69c8541cee0ec462d891178
Tony Km <kimaxhanthony9@gmail.com>
2023-12-06 13:56:31 +0300

    Edit: VoiceSearch

--- src/components/header/VoiceSearch.tsx
+++ src/components/header/VoiceSearch.tsx
@@ -3,14 +3,18 @@ import { useContext } from "react";
 import { headerContext } from "../../layouts/Header";
 
 const VoiceSearch = () => {
-  const { searchIsVisible, handleDisplaySearch } = useContext(
+  const { searchIsVisible, } = useContext(
     headerContext
-  ) as { searchIsVisible: boolean; handleDisplaySearch: () => void };
+  ) as { searchIsVisible: boolean };
 
   return (
     <button className="p-3 grid place-items-center sm:bg-white/20 rounded-full hover:bg-white/20 sm:hover:bg-white/30 transition relative group max-[460px]:hidden">
       <FaMicrophone className="fill-white w-5 h-5" />
-      <small className={`text-white absolute -bottom-12 whitespace-nowrap px-4 py-1.5 bg-slate-500 rounded scale-0 group-hover:scale-100 transition-all z-10  ${searchIsVisible? "hidden": ""}`}>
+      <small
+        className={`text-white absolute -bottom-12 whitespace-nowrap px-4 py-1.5 bg-slate-500 rounded scale-0 group-hover:scale-100 transition-all z-10  ${
+          searchIsVisible ? "hidden" : ""
+        }`}
+      >
         Search with your voice
       </small>
     </button>
